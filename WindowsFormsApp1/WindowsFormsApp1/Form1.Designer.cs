09df02b937647b9f3b7cfec1d21634e7b91a4bd1
koson trachu <koson.trachu@gmail.com>
2018-10-27 21:11:55 +0700

    version 1.1

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(357, 73);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Version 1.0";
+            this.label1.Text = "Version 1.1";
             // 
             // Form1
             // 
